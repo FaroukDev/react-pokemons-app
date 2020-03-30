c1b7b9e311342b2fc5055593d246fd7860f8022c
FaroukDev <fa.elayache1@gmail.com>
2020-03-30 14:15:08 +0200

    add firebase server

---
+++ build/precache-manifest.37d6085dcf098dd6451105c87daecb2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bf3e84395025ec933e8eef7a3aaf6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79cb604fdb1efa9c59c8",
+    "url": "/static/css/main.4254c260.chunk.css"
+  },
+  {
+    "revision": "c1ce159f917bb0e24faa",
+    "url": "/static/js/2.01a327f0.chunk.js"
+  },
+  {
+    "revision": "79cb604fdb1efa9c59c8",
+    "url": "/static/js/main.7cf4e7bb.chunk.js"
+  },
+  {
+    "revision": "a426aa9cdef82f18f533",
+    "url": "/static/js/runtime-main.cc96f8ce.js"
+  }
+]);
